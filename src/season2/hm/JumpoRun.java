21310cfb7716e623ca66634372543615af428afb
Neltharion Earth-Warden <loliikonn@mail.ru>
2023-08-21 21:53:58 +0500

    homwork repaired

--- src/season2/hm/JumpoRun.java
+++ src/season2/hm/JumpoRun.java
@@ -1,7 +1,22 @@
 package season2.hm;
 
 public interface JumpoRun {
-    boolean run(Treadmill t);
+  default boolean run(JumpoRun jr, Treadmill t){
+      System.out.println( jr.getName() +" был бегающим" );
+      System.out.println(t.getDist() < jr.getRUN_DIST() ? "бег был прекрасен" : "бег был ужасен");
+      System.out.println();
+      return t.getDist() < jr.getRUN_DIST();
+    }
 
-    boolean jump(Wall w);
+   default boolean jump(JumpoRun jr, Wall w) {
+       System.out.println(jr.getName() +" был прыгал");
+       System.out.println(w.getHeight() < jr.getJUMP_DIST() ? "прыжок был успешен" : "прыжок был плох");
+       System.out.println();
+       return w.getHeight() < jr.getJUMP_DIST();
+   }
+    public int getJUMP_DIST();
+
+    public int getRUN_DIST();
+
+    public String getName();
 }
