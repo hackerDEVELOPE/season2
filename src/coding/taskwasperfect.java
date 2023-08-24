0dcaa86944a429b2aa95706fc3829f6c664cad8b
Neltharion Earth-Warden <loliikonn@mail.ru>
2023-08-24 22:28:28 +0500

    codewarsing

--- src/coding/taskwasperfect.java
+++ src/coding/taskwasperfect.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class taskwasperfect {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(removeEveryOther(new Object[]{1, null, null})));
-    }
+//        System.out.println(getCount("werklfnweuiguwegwesdf"));
+//        System.out.println(Arrays.toString(removeEveryOther(new Object[]{1, null, null})));}
 
-    public static String doubleChar(String s) {
+/*    public static String doubleChar(String s) {
         StringBuilder s1 = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             s1.append(s.charAt(i)).append(s.charAt(i));
@@ -31,6 +31,40 @@ public class taskwasperfect {
             arr1[k] = arr[i];
         }
         return arr1;
+
+
+ */
+        System.out.println(highAndLow("1 2 3 4 55 -44"));
+    }
+    public static int getCount(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' ||
+                    str.charAt(i) == 'e' ||
+                    str.charAt(i) == 'i' ||
+                    str.charAt(i) == 'o' ||
+                    str.charAt(i) == 'u') {
+                count++;
+            }
+        }
+
+
+        return count;
+
+    }
+
+    public static String highAndLow(String numbers) {
+        String[] arr = numbers.split(" ");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (Integer.parseInt(arr[i]) > max) max = Integer.parseInt(arr[i]);
+            if (Integer.parseInt(arr[i]) < min) min = Integer.parseInt(arr[i]);
+
+
+        }
+        return String.format("%d %d",max, min);
     }
 
 }
